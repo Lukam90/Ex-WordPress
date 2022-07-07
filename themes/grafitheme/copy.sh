74f6183079e8c8afd208030afecac784bce6123c
Lukam90 <lucien.hamm@gmail.com>
2022-07-07 21:40:43 +0200

    MAJ Nv Theme N5

---
+++ themes/grafitheme/copy.sh
@@ -0,0 +1,8 @@
+path="../.."
+target="$HOME/Tutorials/Ex-WordPress"
+
+cp -r $path/themes $target
+
+echo $path
+
+echo "Copie WP"
---
+++ themes/grafitheme/functions.php
@@ -0,0 +1,67 @@
+<?php
+
+function grafitheme_supports() {
+    add_theme_support("title-tag");
+}
+
+add_action("after_setup_theme", "grafitheme_supports");
+
+function grafitheme_register_assets() {
+    wp_register_style("bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css", []);
+    wp_register_script("bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js", ["popper", "jquery"], false, true);
+    wp_register_script("popper", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", [], false, true);
+
+    wp_deregister_script("jquery");
+    wp_register_script("jquery", "https://code.jquery.com/jquery-3.2.1.slim.min.js", [], false, true);
+
+    wp_enqueue_style("bootstrap");
+    wp_enqueue_script("bootstrap");
+}
+
+add_action("wp_enqueue_scripts", "grafitheme_register_assets");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
